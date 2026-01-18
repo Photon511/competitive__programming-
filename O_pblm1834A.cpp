7925695b008cff6a2557c99a9a7550741e8abfbc
Prateek Yadav <prateekyadav511@gmail.com>
2026-01-18 23:36:14 +0530

    todays solving

---
+++ O_pblm1834A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int neg = 0;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            if (x == -1) neg++;
+        }
+
+        int sum = n - 2 * neg;
+        int ops = 0;
+
+        // Step 1: Fix sum
+        if (sum < 0) {
+            int need = (-sum + 1) / 2;  // ceil
+            ops += need;
+            neg -= need;
+        }
+
+        // Step 2: Fix product
+        if (neg % 2 == 1) {
+            ops++;
+        }
+
+        cout << ops << '\n';
+    }
+
+    return 0;
+}
